61d873c9a95340e27990bba30248858ae2b033fc
ssaquisi <ssaquisi@espol.edu.ec>
2026-07-30 15:52:45 -0500

    Scenario 2 in inventory_steps

--- features/steps/inventory_steps.py
+++ features/steps/inventory_steps.py
@@ -13,3 +13,32 @@ def step_impl(context, product):
 def step_impl(context, product):
     assert product in context.inventory, \
         f'{product} was not found in the inventory'
+
+@given('the inventory contains:')
+def step_impl(context):
+    context.inventory = {}
+
+    for row in context.table:
+        product = row["Product"]
+
+        if "Quantity" in row.headings:
+            quantity = int(row["Quantity"])
+        else:
+            quantity = 0
+
+        context.inventory[product] = quantity
+
+    context.output = ""
+
+@when('the user updates "{product}" to quantity "{quantity}"')
+def step_impl(context, product, quantity):
+    if product in context.inventory:
+        context.inventory[product] = int(quantity)
+
+@then('product "{product}" should have quantity "{quantity}"')
+def step_impl(context, product, quantity):
+    assert product in context.inventory, \
+        f'{product} was not found'
+
+    assert context.inventory[product] == int(quantity), \
+        f'Expected quantity {quantity} but got {context.inventory[product]
